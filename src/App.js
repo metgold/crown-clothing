3799c7b8c6a767bc715236f1584e3f425c4e446f
niyi <metric60@gmail.com>
2021-11-14 23:29:42 +0100

    updates on head

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
+import Header from './components/header/header.jsx';
 
 import './App.css';
 
+
 function App() {
   return (
-    <div> 
+    <div>
+      <Header/> 
       <Switch>
           <Route exact path='/' component={HomePage} />
           <Route  path='/shop' component={ShopPage} />
