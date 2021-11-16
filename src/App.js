2e904611a32ca1050f9831209476bb78b895ada8
niyi <metric60@gmail.com>
2021-11-16 23:20:51 +0100

    sign-up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import  SignInAndSignUpPage from './pages/sign_in_and_sign_up/sign_in_and_sign_up';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -24,10 +24,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     });
   }
 
--- src/pages/sign_in_and_sign_up/sign_in_and_sign_up.jsx
+++ src/pages/sign_in_and_sign_up/sign_in_and_sign_up.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign_in/sign_in';
+import SignUp from '../../components/sign_up/sign_up';
 
 import './sign_in_and_sign_up.scss';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
        <SignIn/>
+       <SignUp/>
     </div>
 );
 
